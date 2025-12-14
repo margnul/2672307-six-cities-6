413da9d82827f3af42fffebbadd91546bc95e4b3
Ivan Meynart <ivan.meynart@gmail.com>
2025-12-14 14:24:30 +0300

    Update points.ts

--- src/mocks/points.ts
+++ src/mocks/points.ts
@@ -15,6 +15,10 @@ export const POINTS = [
     title: 'Quiet place with TV',
     lat: 52.3809553943508,
     lng: 4.939309666406198,
+  }, {
+    title: 'Quiet place without TV',
+    lat: 52.52525252525252,
+    lng: 67.67676767676767,
   },
 ];
 
